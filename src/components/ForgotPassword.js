c7b5cb07fcc7fb48afa6e183b5bc43b15bae6742
Upasana0710 <upasanachaudhuri0710@gmail.com>
2023-06-09 21:29:36 +0530

    changed folder structure

---
+++ src/components/ForgotPassword.js
@@ -0,0 +1,119 @@
+import React, { useState } from "react";
+import { Modal } from "@mui/material";
+import styled from "styled-components";
+import { useAuth } from "../contexts/AuthContext";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: #000000a7;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const Card = styled.div`
+  background: ${({ theme }) => theme.card};
+  height: fit-content;
+  width: 420px;
+  padding: 28px 0px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const InnerCard = styled.div`
+  background: ${({ theme }) => theme.bgLight};
+  height: 90%;
+  width: 90%;
+  border-radius: 8px;
+`;
+const Role = styled.div`
+  color: ${({ theme }) => theme.text_primary};
+  flex: 0.5;
+  padding-top: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 520;
+`;
+const Fields = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 20px 0px;
+  width: 100%;
+  gap: 14px;
+`;
+const Field = styled.div`
+  width: 90%;
+  height: 28px;
+  border-radius: 8px;
+  border: 1px solid ${({ theme }) => theme.text_secondary + 99};
+  color: ${({ theme }) => theme.text_secondary};
+  display: flex;
+  align-items: center;
+  padding: 2px 6px;
+`;
+const ButtonContainer = styled.div`
+  width: 94%;
+  background: ${({ theme }) => theme.primary + 99};
+  color: ${({ theme }) => theme.text_primary};
+  font-size: 14px;
+  height: 28px;
+  border-radius: 6px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  cursor: pointer;
+`;
+const ForgotPassword = ({ forgotPassword, setForgotPassword }) => {
+  const [email, setEmail] = useState("");
+  const { forgotpassword } = useAuth();
+  const handleReset = async () => {
+    await forgotpassword(email)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <>
+      <Container>
+        <Card>
+          <InnerCard>
+            <Role>Forgot Password</Role>
+            <Fields>
+              <Field>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  style={{
+                    background: "inherit",
+                    color: "inherit",
+                    outline: "none",
+                    border: "none",
+                    width: "100%",
+                  }}
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </Field>
+              <ButtonContainer onClick={() => handleReset()}>
+                Reset Password
+              </ButtonContainer>
+            </Fields>
+          </InnerCard>
+        </Card>
+      </Container>
+    </>
+  );
+};
+
+export default ForgotPassword;
---
+++ src/components/Menu.js
@@ -0,0 +1,80 @@
+import React from "react";
+import styled from "styled-components";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Link } from 'react-router-dom';
+
+const MenuContainer = styled.div`
+  width: 200px;
+  flex-direction: column;
+  height: 100vh;
+  display: flex;
+  padding: 24px 0px;
+  box-sizing: border-box;
+  align-items: flex-start;
+  background-color: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  @media (max-width: 1100px) {
+    position: fixed;
+    z-index: 1000;
+    width: 100%;
+    max-width: 250px;
+    left: ${({ setMenuOpen }) => (setMenuOpen ? "0" : "-100%")};
+    transition: 0.3s ease-in-out;
+  }
+`;
+const Elements = styled.div`
+  padding: 4px 16px;
+  display: flex;
+  flex-direction: row;
+  box-sizing: border-box;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 12px;
+  cursor: pointer;
+  width: 100%;
+  color: ${({ theme }) => theme.text_secondary};
+  &:hover {
+    background-color: ${({ theme }) => theme.text_secondary + 50};
+  }
+`;
+const NavText = styled.div`
+  padding: 12px 0px;
+`;
+const Name = styled.div`
+color: ${({ theme }) => theme.primary};
+font-size: 20px;
+text-align: center;
+width: 100%;
+margin-bottom: 18px;
+`;
+// eslint-disable-next-line react/prop-types
+const Menu = ({ darkMode, setDarkMode }) => {
+  return (
+    <MenuContainer>
+      <Name>
+        TASKATHON
+      </Name>
+      <Link to="/createtask" style={{ textDecoration: "none", width: "100%" }}>
+        <Elements>
+          <AddCircleOutlineIcon />
+          <NavText>Create Task</NavText>
+        </Elements>
+      </Link>
+      {darkMode ? (
+        <Elements onClick={() => setDarkMode(false)}>
+          <LightModeRoundedIcon />
+          <NavText>Light Mode</NavText>
+        </Elements>
+      ) : (
+        <Elements onClick={() => setDarkMode(true)}>
+          <DarkModeRoundedIcon />
+          <NavText>Dark Mode</NavText>
+        </Elements>
+      )}
+    </MenuContainer>
+  );
+};
+
+export default Menu;
